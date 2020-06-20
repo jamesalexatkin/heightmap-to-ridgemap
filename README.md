7dfb4e9ee5e1e72c1980874ffa2538bd3ce54663
James Atkin <curlyw42@gmail.com>
2020-06-20 21:49:33 +0100

    Added code

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 
 This project enables the creation of ridge maps from height maps. It uses the [`ridge_map`](https://github.com/ColCarroll/ridge_map) Python library.
 
-<p float="left">
-  <img src="https://github.com/jamesalexatkin/heightmap-to-ridgemap/raw/master/heightmaps/ireland.png" width="45%" />
-  <img src="https://github.com/jamesalexatkin/heightmap-to-ridgemap/raw/master/output/ir.png" width="45%" />
-</p>
+<img src="https://github.com/jamesalexatkin/heightmap-to-ridgemap/raw/master/example.png"/>
 
 ## 🗺️ Examples
 
---
+++ heightmap.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+
+def read_image(img_file, num_lines, x_resolution):
+    img = cv2.imread(img_file, 0)
+    
+    height = img.shape[0]
+
+    line_interval = int(height / num_lines)
+
+    image_values = []
+
+    for i in reversed(range(img.shape[0])):
+        if (i % line_interval) == 0:
+            row_values = []
+            for j in range(img.shape[1]):
+                if (j % x_resolution) == 0:
+                    row_values.append(img[i][j])
+            image_values.append(row_values)
+
+    return np.array(image_values)
+
+def get_image_dims(img_file):
+    img = cv2.imread(img_file, 0)
+
+    return img.shape
